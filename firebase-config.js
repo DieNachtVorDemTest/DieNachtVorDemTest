67d70ea3252fd0d98e9490e8eec2e1dd3f265e67
DieNachtVorDemTest <flavio.diluca@students.gymneufeld.ch>
2026-05-12 19:10:48 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,17 @@
-export const firebaseConfig = {
-  apiKey: "DEIN_API_KEY",
-  authDomain: "DEIN_PROJEKT.firebaseapp.com",
-  projectId: "DEIN_PROJEKT",
-  storageBucket: "DEIN_PROJEKT.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abc123"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAPynV_IaVbvl6PAcg8MJB_MLvLvoIUW8E",
+  authDomain: "schoolfools.firebaseapp.com",
+  projectId: "schoolfools",
+  storageBucket: "schoolfools.firebasestorage.app",
+  messagingSenderId: "338912295447",
+  appId: "1:338912295447:web:b8f965686dc4119d777a52"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
